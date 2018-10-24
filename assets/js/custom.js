aaefbcc7d867120d13c5c057cb567b9dc053c93b
demosloft <43844704+demosloft@users.noreply.github.com>
2018-10-24 17:51:09 +0530

    Update new page

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -5,19 +5,5 @@ $(document).ready(function(){
         $(this).siblings().find('.header-sub-dropdown').removeClass('show');
         $(this).addClass('onactive');
     });
-    $("#contact-form").validate({
-			rules: {
-				name: "required",
-				email: {
-					required: true,
-					email: true
-				},
-				message:"required"
-			},
-			messages: {
-				name: "Please enter your name",
-				email: "Please enter a valid email address",
-				message:"Please fill you message here"
-			}
-		});
+    $("#comment-form").validate();
 })
